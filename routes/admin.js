049e6a19b1669b00815a9d73c002018ad7b9c5b2
duknic <njd455@bham.ac.uk>
2015-08-17 18:18:57 +0100

    admin.js now includes a function to calculate current SUS score and pass this to template as variable. This is then displayed in template along with number of submissions

--- routes/admin.js
+++ routes/admin.js
@@ -9,11 +9,15 @@ var app = require('../app');
 router.get('/', stormpath.groupsRequired(['admins']), function (req, res) {
 //router.get('/', function (req, res) {
     //updateLevelsFix(req.client);
-    res.render('admin', {
-        title: 'Admin',
-        pageClass: 'admin',
-        isAdmin: true,
-        givenName: req.user.givenName
+    calculateUsabilityScore(req.db, function(score, numSubmissions){
+        res.render('admin', {
+            title: 'Admin',
+            pageClass: 'admin',
+            isAdmin: true,
+            usabilityScore: score,
+            numSubmissions: numSubmissions,
+            givenName: req.user.givenName
+        });
     });
 });
 
@@ -98,19 +102,48 @@ function getEmailAddresses(client) {
     client.getDirectory('https://api.stormpath.com/v1/applications/49OK2eLCja2aZpbQaaOxYo/accounts', {expand: 'customData'}, function (err, accounts) {
         accounts.forEach(function (acc) {
             //if(acc.customData.total_score <= 0) {
-                fs.appendFile('emails.html', acc.email + '\n', function (err) {
-                })
+            fs.appendFile('emails.html', acc.email + '\n', function (err) {
+            })
             //}
         })
     })
 }
 
+function calculateUsabilityScore(db, callback) {
+    var usability = db.get('usability');
+    // get all usability survey submissions
+    usability.find({}, {}, function (err, docs) {
+        var totalSum = 0.0;
+        // for each submission
+        docs.forEach(function (sub) {
+            var sum = 0;
+            // for every question in submission obj
+            for (var q = 0; q <= 9; q++) {
+                // get question number from JSON key e.g. 'sub3'
+                var qNum = q + 1;
+                // if question number is even
+                if (qNum % 2 == 0) {
+                    sum += 5 - sub['sus' + qNum];
+                } else {
+                    sum += sub['sus' + qNum] - 1;
+                }
+            }
+            // multiple total by 2.5 to get SUS score
+            totalSum += sum * 2.5;
+        })
+        if (typeof callback == 'function') {
+            callback(Math.floor(totalSum / docs.length), docs.length);
+        }
+    })
+}
+
+// this was used to update all users with new levels array
 function updateLevelsFix(client) {
     client.getDirectory('https://api.stormpath.com/v1/applications/49OK2eLCja2aZpbQaaOxYo/accounts', {}, function (err, accounts) {
         accounts.forEach(function (acc) {
             //console.log('getting custom data for ' + acc.givenName)
-            acc.getCustomData(function(err, customData){
-                if(customData.progress.maxLevel > 1) {
+            acc.getCustomData(function (err, customData) {
+                if (customData.progress.maxLevel > 1) {
                     console.log(acc.givenName);
                 }
                 //if (typeof customData.progress.compLevels == 'undefined') {
